32b9dba2a30c9f089b6acb117f18d641a400d015
Zhuojie Zhou <zhouzhuojie@users.noreply.github.com>
2014-05-11 21:21:25 -0400

    Create arch.setup.sh

---
+++ arch.setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install git
+sudo aura -Syu
+sudo aura -S git-core vim ctags openssh wget
+
+# Basic Env
+cd $HOME
+# Make dotfiles dir in setup directory
+sudo aura -S tmux zsh
+rm -rf _dotfiles
+git clone https://github.com/zhouzhuojie/Nitrous.Dotfiles.git _dotfiles
+cp _dotfiles/.bashrc ~/
+cp _dotfiles/.gitconfig ~/
+cp _dotfiles/.tmux.conf ~/
+cp _dotfiles/.vimrc ~/
+rm -rf .oh-my-zsh
+rm .zshrc
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+cp _dotfiles/.arch.zshrc ~/.zshrc
+chsh -s /bin/zsh
+. ~/.zshrc
+
+# Node.js
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+. ~/.nvm/nvm.sh
+nvm install 0.10
+nvm use 0.10
+nvm alias default 0.10
+curl https://install.meteor.com | /bin/sh
+
+# http://jshint.com/
+npm install -g jshint
+npm install -g meteorite
+npm install -g coffee-script
+
+# Python
+sudo aura -S python2 python2-pip python2-numpy python2-scipy python2-networkx
+sudo pip2 install ipython
